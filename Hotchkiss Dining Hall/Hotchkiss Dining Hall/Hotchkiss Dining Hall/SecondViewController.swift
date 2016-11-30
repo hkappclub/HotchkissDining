1fd451c9492eab90130e4ed4693ae3d919355a06
kobiGreene <kobi.greene3@gmail.com>
2016-11-29 19:42:48 -0500

    blah blah

--- Hotchkiss Dining Hall/Hotchkiss Dining Hall/Hotchkiss Dining Hall/SecondViewController.swift
+++ Hotchkiss Dining Hall/Hotchkiss Dining Hall/Hotchkiss Dining Hall/SecondViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var food1 = "Chicken Parm"
         var food2 = "Broccoli"
         var food3 = "Vegan sticks"
+        tableView.delegate = self
         
     }
 
@@ -23,7 +26,22 @@ class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDa
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "blah"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
 
 }
 
