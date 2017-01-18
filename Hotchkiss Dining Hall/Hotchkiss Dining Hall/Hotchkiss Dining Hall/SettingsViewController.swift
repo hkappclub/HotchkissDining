2c677744f911e279bba76439845e7e10b56bce09
Daniel Kim <kdanielive@gmail.com>
2017-01-17 19:34:01 -0500

    Notifications all on doesn't turn off though

--- Hotchkiss Dining Hall/Hotchkiss Dining Hall/Hotchkiss Dining Hall/SettingsViewController.swift
+++ Hotchkiss Dining Hall/Hotchkiss Dining Hall/Hotchkiss Dining Hall/SettingsViewController.swift
@@ -37,20 +37,50 @@ class SettingsViewController: UIViewController, UNUserNotificationCenterDelegate
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func breakfastToggled(_ sender: UISwitch) {
-        if breakfastSwitch.isOn != true {
-            breakfastNotification()
-            breakfastSwitch.setOn(true, animated: true)
+    @IBAction func buttonToggled(_ sender: UISwitch) {
+        if sender.isOn != true {
+            switch sender.tag {
+            case 1:
+                breakfastNotification()
+                break
+            case 2:
+                lunchNotification()
+                break
+            case 3:
+                dinnerNotification()
+                break
+            case 4:
+                dailyOverviewNotification()
+            default:
+                print("error in button toggled!")
+                break
+            }
+            sender.setOn(true, animated: true)
         } else {
+            switch sender.tag {
+            case 1:
             UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: ["breakfastNotification"])
+                break
+            case 2:
+            UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: ["lunchNotification"])
+                break
+            case 3:
+              UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: ["dinnerNotification"])
+                break
+            case 4:
+            UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: ["dailyOverviewNotification"])
+            default:
+                print("error in button toggled!")
+                break
+            }
         }
     }
     
     func breakfastNotification() {
         
         var date = DateComponents()
-        date.hour = 19
-        date.minute = 19
+        date.hour = 7
+        date.minute = 30
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
         
@@ -68,6 +98,77 @@ class SettingsViewController: UIViewController, UNUserNotificationCenterDelegate
             }
         }
     }
+    
+    func lunchNotification() {
+        
+        var date = DateComponents()
+        date.hour = 11
+        date.minute = 30
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Notification Test"
+        content.body = "This is just a test. Here is the body."
+        content.sound = UNNotificationSound.default()
+        
+        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("Uh oh! We had an error: \(error)")
+            }
+        }
+    }
+    
+    func dinnerNotification() {
+        
+        var date = DateComponents()
+        date.hour = 17
+        date.minute = 00
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Notification Test"
+        content.body = "This is just a test. Here is the body."
+        content.sound = UNNotificationSound.default()
+        
+        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("Uh oh! We had an error: \(error)")
+            }
+        }
+    }
+    
+    
+    func dailyOverviewNotification() {
+        
+        var date = DateComponents()
+        date.hour = 7
+        date.minute = 00
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Notification Test"
+        content.body = "This is just a test. Here is the body."
+        content.sound = UNNotificationSound.default()
+        
+        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("Uh oh! We had an error: \(error)")
+            }
+        }
+    }
+    
     /*
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         
